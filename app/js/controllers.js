47201632fe330445177ecdce11aeb3025345955b
Tomasch <gunman@wp.pl>
2014-06-12 21:47:52 +0200

    Jeszcze bez opisu

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,26 +4,35 @@
 
 var phonecatControllers = angular.module('phonecatApp.phonecatControllers', []);
 
-phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
-  function($scope, $http) {
+phonecatControllers.controller('PhoneListCtrl', ['$scope', 'Phone',
+  function($scope, Phone) {
+    $scope.phones = Phone.query();
+    $scope.orderProp = 'age';
+    $scope.passObj = function($ph) {
+        console.log($ph.id);
+	$scope.ph = $ph;
+    }
+  }]);
+/*  function($scope, $http) {
+	console.log('siema');
 
 $http.get("http://localhost:80/").success(function(phones) {
             $scope.phones = phones;
+	console.log('siema');
+	//$scope.phones = $.parseJSON('[{"id":"HkMNOlYcpHg","name":"PSY - HANGOVER feat. Snoop Dogg M\/V","desc":"PSY - HANGOVER feat. Snoop Dogg M\/V]\r\n#PSY #HANGOVER\r\n\r\nAvailable on iTunes @ http:\/\/smarturl.it\/PsyHangoveriT"},{"id":"ypIGeNdJbJ4","name":"some text","desc":"some desc"}]');
         });
     $scope.orderProp = 'name';
   }]);
-
+*/
 phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Phone',
   function($scope, $routeParams, Phone) {
     $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
     });
-
-    $scope.setImage = function(imageUrl) {
-      $scope.mainImageUrl = imageUrl;
-    }
     //$scope.code = 'ypIGeNdJbJ4';
     $scope.code=$routeParams.phoneId;
-    $scope.json = {"id":"ypIGeNdJbJ4","name":" some Name","desc":"some desc"};
+    //$scope.json = {"id":"ypIGeNdJbJ4","name":" some Name","desc":"some desc"};
+    
+console.log('siemabbb' + $scope.phone.name);
 
   }]);
 
@@ -42,6 +51,48 @@ phonecatControllers.directive('myYoutube', function($sce) {
             });
         }
     };
+});
+
+/*phonecatControllers.controller('MainCtrl', function($scope, socket) {
+	$scope.notes = [];
+
+	// Incoming
+	socket.on('onNoteCreated', function(data) {
+		$scope.notes.push(data);
+	});
+
+	socket.on('onNoteDeleted', function(data) {
+		$scope.handleDeletedNoted(data.id);
+	});
+
+	// Outgoing
+	$scope.createNote = function() {
+		var note = {
+			id: new Date().getTime(),
+			title: 'New Note',
+			body: 'Pending'
+		};
+
+		$scope.notes.push(note);
+		socket.emit('createNote', note);
+	};
+
+	$scope.deleteNote = function(id) {
+		$scope.handleDeletedNoted(id);
+
+		socket.emit('deleteNote', {id: id});
+	};
+
+	$scope.handleDeletedNoted = function(id) {
+		var oldNotes = $scope.notes,
+		newNotes = [];
+
+		angular.forEach(oldNotes, function(note) {
+			if(note.id !== id) newNotes.push(note);
+		});
+
+		$scope.notes = newNotes;
+	}
 });
 
 phonecatControllers.directive('stickyNote', function(socket) {
@@ -75,4 +126,4 @@ phonecatControllers.directive('stickyNote', function(socket) {
 			ondelete: '&'
 		}
 	};
-});
+});*/
